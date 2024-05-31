a6330abe7edd9ad6034aab0508f4000fabe7bd8e
Artcraft <arturoao@uma.es>
2024-05-31 18:02:59 +0200

    Added class ComAltura, finished

--- src/main/java/alturas/Mundo.java
+++ src/main/java/alturas/Mundo.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Mundo {
     private List<Pais> paises;
@@ -41,6 +39,70 @@ public class Mundo {
     }
 
     public static <K,V> void presentaEnConsola(Map<K,V> map){
-        
+        for(Map.Entry<K, V> entry : map.entrySet()){
+            System.out.println("Clave: "+entry.getKey()+", Valor: "+entry.getValue());
+        }
     }
+
+    public Map<String, Integer> numeroDePaisesPorContinente(){
+        Map<String, Integer> continentCountMap = new HashMap<>();
+        for(Pais pais : paises){
+            String continente = pais.getContinente();
+            continentCountMap.put(continente, continentCountMap.getOrDefault(continente, 0)+1);
+        }
+        return continentCountMap;
+    }
+
+    public Map<Double, List<Pais>> paisesPorAltura(){
+        Map<Double, List<Pais>> mapeoAltura = new HashMap<>();
+        for(Pais p : paises){
+            double alturaTruncada = Math.floor(p.getAltura()*10/10.0);
+            mapeoAltura.computeIfAbsent(alturaTruncada, k -> new ArrayList<>()).add(p);
+        }
+        return mapeoAltura;
+    }
+
+    public Map<String, Set<Pais>> paisesPorContinente(){
+        Map<String, Set<Pais>> mapeoDeContinente = new TreeMap<>();
+        for(Pais pais : paises){
+            String continent = pais.getContinente();
+            mapeoDeContinente.computeIfAbsent(continent, k-> new TreeSet<>()).add(pais);
+        }
+        return mapeoDeContinente;
+    }
+
+    public Set<Pais> paisesOrdenadosPorAltura(){
+        Set<Pais> ordenadoPorAltura = new TreeSet<>(new CompAltura());
+        ordenadoPorAltura.addAll(paises);
+        return ordenadoPorAltura;
+    }
+
+    public Map<String, Set<Pais>> paisesPorContinenteAltura(){
+        Map<String, Set<Pais>> mapeoContinentes = new TreeMap<>();
+        for(Pais pais : paises){
+            String conti = pais.getContinente();
+            mapeoContinentes.computeIfAbsent(conti, k->new TreeSet<>(new CompAltura())).add(pais);
+        }
+        return mapeoContinentes;
+    }
+
+    public Map<String, Set<Pais>> paisesPorContinenteAlturaDec(){
+        Map<String, Set<Pais>> mapeoDeContinente = new TreeMap<>();
+        for(Pais pais : paises){
+            String continente = pais.getContinente();
+            mapeoDeContinente.computeIfAbsent(continente, k->new TreeSet<>(Comparator.comparing(Pais::getAltura).reversed())).add(pais);
+        }
+        return mapeoDeContinente;
+    }
+
+    public Map<String, Set<Pais>> paisesPorInicial(){
+        Map<String, Set<Pais>> mapeoInicial = new TreeMap<>();
+        for(Pais pais : paises){
+            String inicial = pais.getNombre().substring(0, 1).toUpperCase();
+            mapeoInicial.computeIfAbsent(inicial, k-> new TreeSet<>(Comparator.comparing(Pais::getNombre))).add(pais);
+        }
+        return mapeoInicial;
+    }
+
+    
 }
